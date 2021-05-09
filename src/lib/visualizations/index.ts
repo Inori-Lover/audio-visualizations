3165aaeee9c6cb34d965f327c8c84659c11c4e40
inori-lover <337938188@qq.com>
2021-05-09 14:37:59 +0800

    feat: 理清频率数据来源

--- src/lib/visualizations/index.ts
+++ src/lib/visualizations/index.ts
@@ -1,9 +1,29 @@
+const syncFullPageCanvas = (canvasEle: HTMLCanvasElement) => {
+  canvasEle.width = window.innerWidth;
+  canvasEle.height = window.innerHeight;
+};
+
 export const visualizations = (
   canvasEle: HTMLCanvasElement,
   audioEle: HTMLAudioElement,
 ) => {
-  canvasEle.width = window.innerWidth;
-  canvasEle.height = window.innerHeight;
+  syncFullPageCanvas(canvasEle);
 
-  console.log(canvasEle, audioEle);
+  const ctx = new AudioContext();
+  const analyser = ctx.createAnalyser();
+  const audioSource = ctx.createMediaElementSource(audioEle);
+  // pipe to analyser
+  audioSource.connect(analyser);
+  // pipe to destination（输出端）
+  analyser.connect(ctx.destination);
+
+  const freqData = new Uint8Array(analyser.frequencyBinCount);
+
+  const readFreqData = () => {
+    // 将频率数据填入数组
+    analyser.getByteFrequencyData(freqData);
+    requestAnimationFrame(readFreqData);
+  };
+
+  readFreqData();
 };
